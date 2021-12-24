279ff47f78d1e3f7e45c853225972fb79ed77e56
itsfuz <80732778+itsfuz@users.noreply.github.com>
2021-12-24 20:05:55 +0700

    updated order page

--- resources/views/layouts/truck-cards.blade.php
+++ resources/views/layouts/truck-cards.blade.php
@@ -1,28 +1,33 @@
 <div class="card-group">
-    <div class="card">
+    <div class="card" style="padding:5%">
         <img class="card-img-top" src="{{asset('assets/small truck.jpg')}}" alt="small truck">
         <div class="card-body">
-            <h5 class="card-title">Small Truck</h5>
-            <p class="card-text">DIMENSIONS HERE</p>
-            <p class="card-text">SUPPORTS HERE</p>
-            <p class="card-text">DESCRIPTION HERE</p>
+            <h5 class="card-title"><b>Small Truck</b></h5>
+            <br>
+            <p class="card-text">anak</p>
+            <p class="card-text">anjing</p>
+            <p class="card-text">kimak</p>
         </div>
         <input type="radio" id="small-truck" name="truck_id" value="1" required>
     </div>
-    <div class="card">
+    <br>
+    <div class="card" style="padding:5%">
         <img class="card-img-top" src="{{asset('assets/medium truck.png')}}" alt="medium truck">
         <div class="card-body">
-            <h5 class="card-title">Medium Truck</h5>
+            <h5 class="card-title"><b>Medium Truck</b></h5>
+            <br>
             <p class="card-text">DIMENSIONS HERE</p>
             <p class="card-text">SUPPORTS HERE</p>
             <p class="card-text">DESCRIPTION HERE</p>
         </div>
         <input type="radio" id="medium-truck" name="truck_id" value="2">
     </div>
-    <div class="card">
+    <br>
+    <div class="card" style="padding:5%">
         <img class="card-img-top" src="{{asset('assets/large truck.jpg')}}" alt="large truck">
         <div class="card-body">
-            <h5 class="card-title">Large Truck</h5>
+            <h5 class="card-title"><b>Large Truck</b></h5>
+            <br>
             <p class="card-text">DIMENSIONS HERE</p>
             <p class="card-text">SUPPORTS HERE</p>
             <p class="card-text">DESCRIPTION HERE</p>
